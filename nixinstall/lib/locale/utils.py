19d0e18b30ae2d7cf054da59c1d107f7bdc303e0
Tom van Dijk <18gatenmaker6@gmail.com>
2025-07-13 00:04:12 +0200

    fix: localectl not working on nixos
    Not entirely fixed but at least we can invoke the config driver again

--- nixinstall/lib/locale/utils.py
+++ nixinstall/lib/locale/utils.py
@@ -1,31 +1,51 @@
+from functools import cache
+
 from ..exceptions import ServiceException, SysCallError
 from ..general import SysCommand
+from ..nix import nix_build
 from ..output import error
 
 
+@cache
 def list_keyboard_languages() -> list[str]:
+	kbd = nix_build("kbd")
+	keymap_directory = f'{kbd}/share/keymaps'
+
 	return (
 		SysCommand(
 			'localectl --no-pager list-keymaps',
-			environment_vars={'SYSTEMD_COLORS': '0'},
+			environment_vars={
+				'SYSTEMD_COLORS': '0',
+				'SYSTEMD_KEYMAP_DIRECTORIES': keymap_directory
+			},
 		)
 		.decode()
 		.splitlines()
 	)
 
 
+@cache
 def list_locales() -> list[str]:
-	locales = []
+	# FIXME: see https://github.com/NixOS/nixpkgs/issues/267101#issuecomment-2284844496
+	glibc_locales = nix_build("glibcLocales")
+	locale_archive = f'{glibc_locales}/lib/locale/locale-archive'
 
-	with open('/usr/share/i18n/SUPPORTED') as file:
-		for line in file:
-			if line != 'C.UTF-8 UTF-8\n':
-				locales.append(line.rstrip())
-
-	return locales
+	return (
+		SysCommand(
+			'localectl --no-pager list-locales',
+			environment_vars={
+				'SYSTEMD_COLORS': '0',
+				'LOCALE_ARCHIVE': locale_archive
+			},
+		)
+		.decode()
+		.splitlines()
+	)
 
 
 def list_x11_keyboard_languages() -> list[str]:
+	# TODO: probe whether this works on a minimal ISO and what we would need to
+	# do to fix it
 	return (
 		SysCommand(
 			'localectl --no-pager list-x11-keymap-layouts',
@@ -84,8 +104,12 @@ def set_kb_layout(locale: str) -> bool:
 			error(f'Invalid keyboard locale specified: {locale}')
 			return False
 
+		# Failed to set keymap: Changing system settings via systemd is not supported on NixOS.
+		# FIXME: it cannot be done in-place on the system. Would we actually
+		# have to rebuild the system or can we change it on-the-fly in SOME
+		# way?
 		try:
-			SysCommand(f'localectl set-keymap {locale}')
+			SysCommand(f'loadkeys {locale}')
 		except SysCallError as err:
 			raise ServiceException(f"Unable to set locale '{locale}' for console: {err}")
 
---
+++ nixinstall/lib/nix/__init__.py
@@ -0,0 +1,12 @@
+from ..general import SysCommand
+
+
+# returns outpath
+def nix_build(package_name: str) -> str:
+	return (
+		SysCommand(['nix-build', '<nixpkgs>', '--no-out-link', '-A', package_name])
+		.decode()
+		.splitlines()[0]
+	)
+
+
